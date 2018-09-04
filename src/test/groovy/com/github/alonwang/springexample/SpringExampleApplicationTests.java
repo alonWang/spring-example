72d7d19fa9dbfbcce9b1f7161a8794734ede086f
weilong.wang <weilong.wang@jiamiantech.com>
2018-09-04 10:11:26 +0800

    add test support

--- src/test/groovy/com/github/alonwang/springexample/SpringExampleApplicationTests.java
+++ src/test/groovy/com/github/alonwang/springexample/SpringExampleApplicationTests.java
@@ -5,6 +5,7 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+//FIXME spock not run
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SpringExampleApplicationTests {
