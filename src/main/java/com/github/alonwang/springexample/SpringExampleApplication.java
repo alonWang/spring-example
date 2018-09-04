b250dff23722c54642681963cb5247972fd6802b
weilong.wang <weilong.wang@jiamiantech.com>
2018-09-04 09:24:03 +0800

    init

---
+++ src/main/java/com/github/alonwang/springexample/SpringExampleApplication.java
@@ -0,0 +1,12 @@
+package com.github.alonwang.springexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringExampleApplication.class, args);
+	}
+}
